72648c71cbf5d312bc508ffb5e2af49dd9cccb7f
Hju Kneyck Flores <hjucode@gmail.com>
2022-12-08 18:10:41 +0800

    Enable image upload on add product

--- screens/addProduct.js
+++ screens/addProduct.js
@@ -1,116 +1,212 @@
 import React from 'react';
-import {Text, View,StyleSheet,Image, TouchableOpacity, TextInput, ScrollView, SafeAreaView} from 'react-native';
+import {
+  Text,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Image,
+  Alert,
+} from 'react-native';
 import SelectDropdown from 'react-native-select-dropdown';
-
-/*Icons Library-Start*/
-import AntDesign from 'react-native-vector-icons/AntDesign';
-import Entypo from 'react-native-vector-icons/Entypo';
-import Feather from 'react-native-vector-icons/Feather';
-import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
-import Fontisto from 'react-native-vector-icons/Fontisto';
-import Foundation from 'react-native-vector-icons/Foundation';
-import MCI from 'react-native-vector-icons/MaterialCommunityIcons';
-import MI from 'react-native-vector-icons/MaterialIcons';
-/*Icons Library-End*/
+import * as ImagePicker from 'expo-image-picker';
 
-const AddProduct = ({navigation}) => {
-  const [selected,setSelected] = React.useState("");
-  const data = [
-    {key:'1',value:'Vegetable'},
-    {key:'2',value:'Fruit'},];
-  const countries = ["Vegetable", "Fruit"]
-  return(
+const countries = ['Vegetable', 'Fruit'];
+
+async function base64File(url) {
+  const data = await fetch(url);
+  const blob = await data.blob();
+  return new Promise(resolve => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      const base64data = reader.result;
+      resolve(base64data);
+    };
+  });
+}
+
+export default function AddProduct({navigation}) {
+  const [selectedImage, setSelectedImage] = React.useState([]);
+  const [productInfo, setProductInfo] = React.useState({
+    image: '',
+    name: '',
+    price: '',
+    quantity: '',
+    description: '',
+    category: '',
+  });
+
+  const launchImageLibraryHandler = () => {
+    ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      quality: 1,
+    }).then(selectedImg => {
+      setSelectedImage(selectedImg.assets);
+      setProductInfo({...productInfo, image: selectedImg.assets[0].uri});
+      // base64File(selectedImg.assets[0].uri).then(res => {
+      //   setProductInfo({...productInfo, image: res});
+      // });
+    });
+  };
+
+  const handleAddProduct = () => {
+    const formData = new FormData();
+    formData.append('image', {
+      uri: productInfo.image,
+      type: 'image/jpeg',
+      name: productInfo.image.substring(
+        productInfo.image.lastIndexOf('/') + 1,
+        productInfo.image.length,
+      ),
+    });
+    formData.append('name', productInfo.name);
+    formData.append('price', productInfo.price);
+    formData.append('quantity', productInfo.quantity);
+    formData.append('category', productInfo.category);
+    formData.append('description', productInfo.description);
+    formData.append('user_id', global.id);
+    formData.append(
+      'seller_name',
+      `${global.firstname} ${global.middlename} ${global.lastname}`,
+    );
+
+    fetch('http://localhost:8000/api/products', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+      body: formData,
+    })
+      .then(res => res.json())
+      .then(data => {
+        if (data.status === 200) {
+          navigation.navigate('Products');
+        } else {
+          Alert.alert('Error', 'Could not add product');
+          console.error(data);
+        }
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  };
+
+  return (
     <ScrollView contentContainerStyle={styles.contentContainer}>
-    <View style = {styles.ground}>
-    <View style = {styles.foreground}>
-    <View style = {[styles.mPBox]}>
-        <TouchableOpacity>
-        <Text style = {styles.leftIcon}>
-            <FontAwesome5 name="arrow-left" color={'black'} size={25} iconStyle={''} onPress={()=> navigation.navigate('Products')}/>
-        </Text>
-        </TouchableOpacity>
-    <Text style = {styles.edit}>Add New Product</Text>
-    </View>
-      <Text style = {styles.addtext}>Add product image</Text>
-      <Text style = {styles.text}>Upload an image of your product</Text>
-
-      <View style={styles.circle}>
-      <TouchableOpacity style = {styles.addButton} >
-            <FontAwesome5 name='plus' color={'white'} size={30} iconStyle={''}/>
+      <View style={styles.ground}>
+        <View style={styles.foreground}>
+          <View style={[styles.mPBox]}>
+            <TouchableOpacity>
+              <Text style={styles.leftIcon}>
+                <FontAwesome5
+                  name="arrow-left"
+                  color={'black'}
+                  size={25}
+                  iconStyle={''}
+                  onPress={() => navigation.navigate('Products')}
+                />
+              </Text>
             </TouchableOpacity>
+            <Text style={styles.edit}>Add New Product</Text>
+          </View>
+          <Text style={styles.addtext}>Add product image</Text>
+          <Text style={styles.text}>Upload an image of your product</Text>
+          {selectedImage.length > 0 ? (
+            <Image
+              style={{width: 150, height: 150}}
+              source={{uri: selectedImage[0]?.uri}}
+            />
+          ) : (
+            <TouchableOpacity
+              style={styles.circle}
+              onPress={launchImageLibraryHandler}>
+              <View style={styles.addButton}>
+                <FontAwesome5 name="plus" color={'white'} size={30} />
+              </View>
+            </TouchableOpacity>
+          )}
+
+          <Text style={styles.addtext}>Add product details</Text>
+          <Text style={styles.text}>Select product category</Text>
+          <SelectDropdown
+            defaultButtonText={' '}
+            buttonStyle={styles.dropdown1BtnStyle}
+            buttonTextStyle={styles.dropdown1BtnTxtStyle}
+            dropdownStyle={styles.dropdown1DropdownStyle}
+            rowStyle={styles.dropdown1RowStyle}
+            rowTextStyle={styles.dropdown1RowTxtStyle}
+            data={countries}
+            onSelect={selectedItem => {
+              setProductInfo({...productInfo, category: selectedItem});
+            }}
+            buttonTextAfterSelection={(selectedItem, index) => {
+              // text represented after item is selected
+              // if data array is an array of objects then return selectedItem.property to render after item is selected
+              return selectedItem;
+            }}
+            rowTextForSelection={(item, index) => {
+              // text represented for each item in dropdown
+              // if data array is an array of objects then return item.property to represent item in dropdown
+              return item;
+            }}
+          />
+          <Text style={styles.text}>Product Name</Text>
+          <TextInput
+            style={styles.input}
+            onChange={e =>
+              setProductInfo({...productInfo, name: e.nativeEvent.text})
+            }></TextInput>
+
+          <Text style={styles.text}>Description</Text>
+          <TextInput
+            style={styles.input}
+            onChange={e =>
+              setProductInfo({
+                ...productInfo,
+                description: e.nativeEvent.text,
+              })
+            }></TextInput>
+
+          <Text style={styles.text}>Product Price</Text>
+          <TextInput
+            placeholder="Price in Peso (ex. 20.00)"
+            style={styles.input}
+            keyboardType="numeric"
+            onChange={e =>
+              setProductInfo({...productInfo, price: e.nativeEvent.text})
+            }></TextInput>
+
+          <Text style={styles.text}>Product Quantity</Text>
+          <TextInput
+            placeholder="Quantity in Kilograms"
+            style={styles.input}
+            keyboardType="numeric"
+            onChange={e =>
+              setProductInfo({...productInfo, quantity: e.nativeEvent.text})
+            }></TextInput>
+
+          <View style={styles.bottom}>
+            <TouchableOpacity style={styles.button} onPress={handleAddProduct}>
+              <Text style={styles.buttonText}>ADD PRODUCT</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
       </View>
-
-      <Text style = {styles.addtext}>Add product details</Text> 
-      <Text style = {styles.text}>Select product category</Text>
-      <SelectDropdown
-        defaultButtonText={' '}
-        buttonStyle={styles.dropdown1BtnStyle}
-        buttonTextStyle={styles.dropdown1BtnTxtStyle}
-        dropdownStyle={styles.dropdown1DropdownStyle}
-        rowStyle={styles.dropdown1RowStyle}
-        rowTextStyle={styles.dropdown1RowTxtStyle}
-          data={countries}
-          onSelect={(selectedItem, index) => {
-            console.log(selectedItem, index)
-          }}
-          buttonTextAfterSelection={(selectedItem, index) => {
-            // text represented after item is selected
-            // if data array is an array of objects then return selectedItem.property to render after item is selected
-            return selectedItem
-          }}
-          rowTextForSelection={(item, index) => {
-            // text represented for each item in dropdown
-            // if data array is an array of objects then return item.property to represent item in dropdown
-            return item
-          }}
-        />
-        <Text style = {styles.text}>Product Name</Text>
-       <TextInput 
-      style = {styles.input}>
-      </TextInput>
-
-      <Text style = {styles.text}>Product Price</Text>
-      <TextInput 
-      placeholder='Price in Peso (ex. 20.00)'
-      style = {styles.input}
-      keyboardType='numeric'>
-      </TextInput>
-
-      <Text style = {styles.text}>Product Quantity</Text>
-      <TextInput 
-      placeholder='Quantity in Kilograms'
-      style = {styles.input}
-      keyboardType='numeric'>
-      </TextInput>
-
-      <View style={styles.bottom}>
-      <TouchableOpacity 
-      style = {styles.button} onPress={()=> navigation.navigate('Products')}>
-        <Text 
-        style = {styles.buttonText}>
-          ADD PRODUCT</Text>
-      </TouchableOpacity>
-      </View>
-      
-    </View>
-    </View>
     </ScrollView>
   );
 }
 
 const styles = StyleSheet.create({
   contentContainer: {
-    flex: 1,
-    justifyContent: 'flex-end',
     color: '#F4F4F4',
   },
-  ground:{
+  ground: {
     backgroundColor: '#F4F4F4',
-    flex:1,
-    justifyContent: 'center',
   },
-  foreground:{
+  foreground: {
     flex: 1,
     flexDirection: 'column',
     alignContent: 'space-around',
@@ -118,25 +214,25 @@ const styles = StyleSheet.create({
     marginLeft: 30,
     marginRight: 30,
   },
-  mPBox:{
-    flexDirection: "row",
+  mPBox: {
+    flexDirection: 'row',
     flexWrap: 'nowrap',
     alignItems: 'center',
     justifyContent: 'flex-start',
-},
-leftIcon:{
-    justifyContent:'flex-start',
+  },
+  leftIcon: {
+    justifyContent: 'flex-start',
     marginLeft: '5%',
-},
-  edit:{
+  },
+  edit: {
     color: 'green',
     fontSize: 24,
     fontWeight: 'bold',
     textAlign: 'left',
     marginTop: 5,
-    marginLeft: 15,   
+    marginLeft: 15,
   },
-  addtext:{
+  addtext: {
     color: 'green',
     fontWeight: 'bold',
     fontSize: 18,
@@ -146,25 +242,25 @@ leftIcon:{
     width: 150,
     height: 150,
     borderRadius: 10,
-    backgroundColor: "#388E3C",
+    backgroundColor: '#388E3C',
     marginTop: 10,
   },
-  addButton:{
+  addButton: {
     height: '100%',
     width: '100%',
     alignSelf: 'auto',
     justifyContent: 'center',
     alignItems: 'center',
   },
-  text:{
+  text: {
     fontWeight: 'bold',
     color: 'black',
     fontSize: 16,
   },
-  inputsBox:{
+  inputsBox: {
     marginBottom: 0,
   },
-  input:{
+  input: {
     backgroundColor: '#D9D9D9',
     borderRadius: 4,
     flexDirection: 'row',
@@ -173,12 +269,12 @@ leftIcon:{
     padding: 10,
     fontWeight: 'bold',
   },
-  bottom:{
+  bottom: {
     flex: 1,
     justifyContent: 'flex-end',
-    marginBottom: 15
+    marginBottom: 15,
   },
-  button:{
+  button: {
     backgroundColor: 'green',
     borderRadius: 10,
     marginTop: 20,
@@ -187,7 +283,7 @@ leftIcon:{
     alignItems: 'center',
     alignSelf: 'stretch',
   },
-  buttonText:{
+  buttonText: {
     color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
@@ -200,23 +296,20 @@ leftIcon:{
     marginBottom: 15,
   },
   dropdown1BtnTxtStyle: {
-    color: '#444', 
-    textAlign: 'left', 
-    fontWeight: 'bold'
-  },
-  dropdown1DropdownStyle: {
-    backgroundColor: 'white'
-  },
-  dropdown1RowStyle: {
-    backgroundColor: '#F4F4F4', 
-    borderBottomColor: 'green'
-  },
-  dropdown1RowTxtStyle: {
-    color: 'black', 
+    color: '#444',
     textAlign: 'left',
     fontWeight: 'bold',
   },
-
-})
-
-export default AddProduct;
+  dropdown1DropdownStyle: {
+    backgroundColor: 'white',
+  },
+  dropdown1RowStyle: {
+    backgroundColor: '#F4F4F4',
+    borderBottomColor: 'green',
+  },
+  dropdown1RowTxtStyle: {
+    color: 'black',
+    textAlign: 'left',
+    fontWeight: 'bold',
+  },
+});
--- screens/signIn.js
+++ screens/signIn.js
@@ -1,117 +1,105 @@
 import React, {Component, useState} from 'react';
-import {Text, View,StyleSheet,Image, TouchableOpacity, TextInput} from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  Alert,
+} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 const SignIn = ({navigation}) => {
-  
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
   const loginSeller = async () => {
-    await fetch('http://10.0.2.2:8000/api/login', {
-        method: 'POST',
-        headers: {
-          'Accept': 'application/json',
-          'Content-Type': 'application/json'
-
-        },
-        body: JSON.stringify({'username': username, 'password': password})
-      }).then(res=> res.json())
+    await fetch('http://localhost:8000/api/login', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({username: username, password: password}),
+    })
+      .then(res => res.json())
       .then(resData => {
-        if ('error' in resData) {
-          alert('Username or Password')
-          console.log(resData)
-        }
-        else 
-        {
-          global.id = resData.id
-          global.firstname = resData.firstname
-          global.middlename = resData.middlename
-          global.lastname = resData.lastname
-          global.username = resData.username
-          global.email = resData.email
-          global.username = resData.username
-          global.mobilephone = resData.mobilephone
+        if ('notVerified' in resData) {
+          Alert.alert('Error', 'Account not verified', [
+            {
+              text: 'OK',
+            },
+          ]);
+          console.error(resData);
+        } else if ('error' in resData) {
+          Alert.alert('Error', 'Invalid username or password', [
+            {
+              text: 'OK',
+            },
+          ]);
+          console.error(resData);
+        } else {
+          global.id = resData.id;
+          global.firstname = resData.firstname;
+          global.middlename = resData.middlename;
+          global.lastname = resData.lastname;
+          global.username = resData.username;
+          global.email = resData.email;
+          global.username = resData.username;
+          global.mobilephone = resData.mobilephone;
           setPassword('');
-          setUsername(''),
-          navigation.navigate('Tabs')
+          setUsername('');
+          navigation.navigate('Tabs');
         }
-      })
-  }
+      });
+  };
 
-  /*const handleCheckUsername = text => {
-    if(text.length < 1)
-    {
-      setCheckValidUser(true);
-    }
-    else
-    {
-      setCheckValidUser(false);
-    }
-  }
+  return (
+    <View style={styles.ground}>
+      <View style={styles.foreground}>
+        <Text style={styles.welcome}>Welcome,</Text>
+        <Text style={styles.continue}>Sign-in as Seller</Text>
 
-  const handleCheckConfirmPassword = text => {
-    if(text.length <1)
-    {
-      setCheckValidPassword(true);
-    }
-    else
-    {
-      setCheckValidPassword(false);
-    }
-  }*/
+        <View style={styles.inputsBox}>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setUsername(text)}
+            placeholder="Enter Username"
+            placeholderTextColor="gray"
+          />
 
-  return(
-    <View style = {styles.ground}>
-    <View style = {styles.foreground}>
-      <Text style = {styles.welcome}>Welcome,</Text>
-      <Text style = {styles.continue}>Sign-in as Seller</Text>
+          <TextInput
+            style={styles.input}
+            onChangeText={text => setPassword(text)}
+            placeholder="Password"
+            secureTextEntry={true}
+            placeholderTextColor="gray"
+          />
+        </View>
 
-      <View style = {styles.inputsBox}>
-      <TextInput 
-      style = {styles.input} 
-      onChangeText = { (text) => setUsername(text) }
-      placeholder='Enter Username'
-      placeholderTextColor= 'gray'
-      />
+        <TouchableOpacity style={styles.button} onPress={loginSeller}>
+          <Text style={styles.buttonText}>LOG IN</Text>
+        </TouchableOpacity>
 
-      
-      <TextInput 
-      style = {styles.input} 
-      onChangeText = { (text) =>  setPassword(text)}
-      placeholder='Password'
-      secureTextEntry={true}
-      placeholderTextColor= 'gray'
-      />
-
+        <Text style={styles.ask}>Don't have account?</Text>
+        <TouchableOpacity
+          onPress={() => navigation.navigate('SellerRegistration')}>
+          <Text style={styles.registerButton}>Register Here</Text>
+        </TouchableOpacity>
       </View>
-
-      <TouchableOpacity 
-      style = {styles.button}
-      onPress={ loginSeller } >
-        <Text 
-        style = {styles.buttonText}>
-          LOG IN</Text>
-      </TouchableOpacity>
-      
-      <Text style = {styles.ask}>Don't have account?</Text>
-      <TouchableOpacity onPress={ () => navigation.navigate('SellerRegistration')}>
-        <Text style = {styles.registerButton}>
-          Register Here</Text>
-          </TouchableOpacity>
-    </View>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
-  ground:{
+  ground: {
     backgroundColor: '#F4F4F4',
-    flex:1,
+    flex: 1,
     justifyContent: 'center',
   },
-  foreground:{
+  foreground: {
     flex: 1,
     flexDirection: 'column',
     alignContent: 'space-around',
@@ -119,19 +107,19 @@ const styles = StyleSheet.create({
     marginLeft: 30,
     marginRight: 30,
   },
-  welcome:{
+  welcome: {
     color: 'green',
     fontSize: 28,
     fontWeight: 'bold',
   },
-  continue:{
+  continue: {
     color: 'black',
     fontSize: 22,
   },
-  inputsBox:{
+  inputsBox: {
     marginTop: 70,
   },
-  input:{
+  input: {
     backgroundColor: 'white',
     borderColor: 'green',
     borderWidth: 1,
@@ -141,7 +129,7 @@ const styles = StyleSheet.create({
     fontSize: 18,
     padding: 10,
   },
-  button:{
+  button: {
     backgroundColor: 'green',
     borderRadius: 30,
     marginTop: 50,
@@ -151,22 +139,22 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  buttonText:{
+  buttonText: {
     color: 'white',
     fontSize: 18,
     fontWeight: 'bold',
   },
-  ask:{
+  ask: {
     textAlign: 'center',
     marginTop: 150,
     fontSize: 16,
   },
-  registerButton:{
+  registerButton: {
     textAlign: 'center',
     color: 'green',
     fontWeight: 'bold',
     fontSize: 16,
   },
-})
+});
 
 export default SignIn;
